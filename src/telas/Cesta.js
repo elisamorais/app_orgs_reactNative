65fe6437569bac1b78f90cbc655c0a7180f5a46e
Elisa Morais <elisa.morais74@gmail.com>
2022-09-16 20:48:40 -0300

    Continuação aula: Imagem e estilos.

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,30 @@
 import React from "react";
-import { Text } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
-    return <Text>Cesta</Text>
+    return <>
+    <Image source={topo} style={estilos.topo} />
+    <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+    </>
 }
+
+const estilos = StyleSheet.create ({
+    topo: {
+        width:"100 %",
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        position: 'absolute',
+        textAlign: 'center',
+        width: '100%',
+        fontSize: 25,
+        lineHeight: 26,
+        color: 'white',
+        fontWeight: 'bold',
+        padding: 16
+    }
+});
