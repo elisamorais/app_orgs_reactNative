203cf2f6fbc5258ec9494d73ef54d95b5b1e29ad
Elisa Morais <elisa.morais74@gmail.com>
2022-09-17 19:20:48 -0300

    continuação aula: componente de texto - ok

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
+import Texto from '../componentes/Texto';
+
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
 
@@ -9,17 +11,17 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
     return <>
     <Image source={topo} style={estilos.topo} />
-    <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+    <Texto style={estilos.titulo}>Detalhes da Cesta</Texto>
 
     <View style={estilos.cesta}>
-        <Text style={estilos.nome}>Cesta de Verduras</Text>
+        <Texto style={estilos.nome}>Cesta de Verduras</Texto>
         <View style={estilos.fazenda}>
         <Image source={logo} style={estilos.imagemFazenda} />
-        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
         </View>
         
-        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente diretamente direto
-            para a sua cozinha.</Text>
+        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente diretamente direto
+            para a sua cozinha.</Texto>
         <Text style={estilos.preco}>R$ 40,00</Text>
     </View>
     </>
@@ -37,8 +39,8 @@ const estilos = StyleSheet.create ({
         fontSize: 25,
         lineHeight: 26,
         color: 'white',
-        fontWeight: 'bold',
-        padding: 16
+        padding: 16,
+        fontFamily: "MontserratBold",
     },
     cesta: {
         paddingVertical: 8,
@@ -48,7 +50,7 @@ const estilos = StyleSheet.create ({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontWeight: 'bold'
+        fontWeight: "bold"
     },
     fazenda: {
         flexDirection: 'row',
